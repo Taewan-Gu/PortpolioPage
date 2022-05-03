d8c72bc01b15665570dca0f7638df39d260fe4ff
fksk94 <fksk94@gmail.com>
2022-05-03 21:20:20 +0900

    refactor: 💡 mobile resizing optimization

--- front/src/components/portpolio/Profile.vue
+++ front/src/components/portpolio/Profile.vue
@@ -18,6 +18,30 @@ export default {
   border-radius: 250px;
 }
 
+@media screen and (max-width: 800px) {
+  .profile-image {
+    width: 220px;
+    height: 220px;
+    border-radius: 220px;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .profile-image {
+    width: 200px;
+    height: 200px;
+    border-radius: 200px;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .profile-image {
+    width: 180px;
+    height: 180px;
+    border-radius: 180px;
+  }
+}
+
 .intro {
   margin-top: 30px;
   font-size: 18px;
