8f02c6fed6a77f0fc8f6c40c81b527816cdb29c2
fksk94 <fksk94@gmail.com>
2022-05-15 16:03:46 +0900

    feat: 🎸 remove html resume & english resume

--- back/routers/resume.py
+++ back/routers/resume.py
@@ -12,7 +12,4 @@ parent_route = path.dirname(path.abspath(path.dirname(__file__)))
 @router.get("/download", description="resume download")
 def get_resume():
     return FileResponse(f"{parent_route}/assets/resume.pdf", filename="resume.pdf")
-
-@router.get("/html", description="resume html")
-def get_resume():
-    return FileResponse(f"{parent_route}/assets/resume.html")
+    
